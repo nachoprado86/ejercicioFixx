032d0167c857cfafae9d5abd2f085a3637793c88
nachoprado86 <fonsvitae.spain@gmail.com>
2023-05-28 12:51:57 +0200

    ejercicio hecho

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-lalalalalala
+let fizzBuzz = () => {
+    for (let index = 1; index < 1001; index++) {
+        if (index%3 == 0) {
+            return Fizz
+            
+        }
+        if (index%5 == 0) {
+            return Buz
+            
+        }
+        if (index%15 == 0) {
+            return FizzBuzz
+            
+        }
+        else return index
+    }
+}
